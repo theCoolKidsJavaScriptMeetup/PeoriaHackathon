3734b4a4cd4bba3013537aae2027d0992728c0d0
Daniel Hart <me@danielhart.co>
2017-06-17 18:29:30 -0500

    Changed name and CSS changes
     - Changed name to ‘Peoria Hacks’ and changed logo
     - Added some CSS, created a button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 class App extends Component {
@@ -8,11 +8,13 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h1>Peoria Hacks</h1>
+          <div className="button"><a href="#">Sign up now</a></div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <div className="Content">
+          <h2>Welcome!</h2>
+        </div>
       </div>
     );
   }
