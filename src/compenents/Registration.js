8c1d06957080e2158cfc2809bfec6ebd343106d4
Deploy <deploy@deploy.com>
2017-06-26 19:18:34 -0500

    Get form to send data to firebase

--- src/compenents/Registration.js
+++ src/compenents/Registration.js
@@ -23,27 +23,34 @@ export default class Registration extends Component {
   constructor (props, context) {
     super(props, context)
     this.state = {
-      users: []
+      users: [],
+      email: 'Email',
+      firstName: 'First Name',
+      lastName: 'Last Name',
+      skill: 'Select Skill'
     }
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.newUser = this.newUser.bind(this);
   }
 
   componentWillMount () {
     const ref = Firebase.database().ref('users');
     this.bindAsArray(ref, 'users');
-    this.newUser('Tori', 'Huang', 'tori.huang@rocketsled.io', 'Developer')
+    // this.newUser('Tori', 'Huang', 'tori.huang@rocketsled.io', 'Developer')
   }
 
   componentWillUnmount () {
     this.unbind('users')
   }
 
-  newUser(firstName, lastName, email, skill) {
+  newUser() {
     // A post entry.
     var userData = {
-      firstName,
-      lastName,
-      email,
-      skill,
+      firstName: this.state.firstName,
+      lastName: this.state.lastName,
+      email: this.state.email,
+      skill: this.state.skill,
     };
 
     // Get a key for a new Post.
@@ -57,10 +64,50 @@ export default class Registration extends Component {
       .catch(err => console.log('err', err));
   }
 
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  renderForm() {
+    return (
+      <div style={{ flexDirection: 'column', backgroundColor: 'purple' }}>
+        <form onSubmit={this.newUser}>
+          <label>
+            First Name
+            <input type="text" name="firstName" value={this.state.firstName} onChange={this.handleInputChange} />
+          </label>
+          <label>
+            Last Name
+            <input type="text" name="lastName" value={this.state.lastName} onChange={this.handleInputChange} />
+          </label>
+          <label>
+            Email
+            <input type="text" name="email" value={this.state.email} onChange={this.handleInputChange} />
+          </label>
+          <select value={this.state.skill} name="skill" onChange={this.handleInputChange}>
+            <option value='Select Skill' disabled>Select Skill</option>
+            <option value="Other">Other</option>
+            <option value="Marketing">Marketing</option>
+            <option value="Developer">Developer</option>
+            <option value="Designer">Designer</option>
+          </select>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    )
+  }
+
   render() {
     return (
       <div className='Content'>
         <h2>Get Registered, like a boss</h2>
+        {this.renderForm()}
       </div>
     );
   }
