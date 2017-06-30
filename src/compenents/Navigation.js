15aa4aa7ec033a9be824711a0d0426f046ac0da3
Daniel Hart <me@danielhart.co>
2017-06-30 17:11:51 -0500

    Added navigation bar

---
+++ src/compenents/Navigation.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
+import Main from '../compenents/Main';
+import Sponsors from '../compenents/Sponsors';
+import Registration from '../compenents/Registration';
+
+import logo from '../logo.png';
+import '../App.css';
+import '../Navigation.css';
+
+export default class Navigation extends Component {
+  render() {
+    return (
+      <div className="Navigation">
+        <div className="Nav_logo">
+          <img src={logo} className="Nav_Logo" alt="logo" />
+        </div>
+
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="registration">Registration</a></li>
+            <li><a href="/#faq">FAQ</a></li>
+            <li><a href="sponsors">Sponsors</a></li>
+        </ul>
+      </div>
+    );
+  }
+}
