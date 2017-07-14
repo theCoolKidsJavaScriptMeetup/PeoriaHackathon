cb2eab40919eecc440410ab52a880658ff9335f4
Daniel Hart <me@danielhart.co>
2017-07-14 15:12:09 -0500

    Style agenda on homepage

--- src/compenents/Main.js
+++ src/compenents/Main.js
@@ -19,49 +19,43 @@ export default class Main extends Component {
 		    	</div>
 
 		    	<div className="agenda">
+		    		<h2 align="center">Agenda</h2>
+		    		<h3>Friday Night Meet @ ___ (local watering hole)</h3>
 			    	<ul>
-			    		<li>Friday Night Meet @ ___ (local watering hole)</li>
-			    		<li>6-6:30 PM: Check-in, pick up a name tag, and grab a drink (if you wish)!</li>
+			    		<li><span>6-6:30 PM: Check-in, pick up a name tag, and grab a drink (if you wish)!</span></li>
 
-						<li>6:30-7 PM: Welcome from the i-team and Q & A session about the Hackathon
-						<br/>Use this time to gather with your team or find a team if you are a solo participant looking to join a team. The i-team will also be open to answering any questions about the hackathon during this time</li>
+						<li><span>6:30-7 PM: Welcome from the i-team and Q & A session about the Hackathon</span>
+						<br/><br/>Use this time to gather with your team or find a team if you are a solo participant looking to join a team. The i-team will also be open to answering any questions about the hackathon during this time</li>
 
-						<li>7-8 PM: Mingle with fellow hackers!
-						<br/>Use this time to brainstorm with your team, welcome new members to your team, or just meet some new people and have fun!</li>
+						<li><span>7-8 PM: Mingle with fellow hackers!</span>
+						<br/><br/>Use this time to brainstorm with your team, welcome new members to your team, or just meet some new people and have fun!</li>
 					</ul>
-					
+					<h3>Saturday Meet @ the Civic Center Wireless Lobby</h3>
 					<ul>
-						<li>Saturday Meet @ the Civic Center Wireless Lobby</li>
+						<li><span>8-8:30 AM: Check-in and Breakfast</span>
+						<br/><br/>Stop at the registration table to check-in, grab a name tag, make sure all members of your team are registered, and find out where your team’s work space is located. Teams will have the opportunity to move around once the Hackathon period begins. Be sure to grab some  breakfast and check out the SWAG table!</li>
 
-						<li>8-8:30 AM: Check-in and Breakfast
-						<br/>Stop at the registration table to check-in, grab a name tag, make sure all members of your team are registered, and find out where your team’s work space is located. Teams will have the opportunity to move around once the Hackathon period begins. Be sure to grab some  breakfast and check out the SWAG table!</li>
+						<li><span>8:30-9 AM: Opening Remarks from the i-team and City of Peoria IT Department</span>
+						<br/><br/>A word from our sponsors. Introduction of the  Hackathon Challenge Statement</li>
 
-						<li>8:30-9 AM: Opening Remarks from the i-team and City of Peoria IT Department
-						<br/>A word from our sponsors. Introduction of the  Hackathon Challenge Statement</li>
+						<li><span>9 AM: HACKATHON BEGINS – GET HACKING!</span>
+							<br/><br/>Registration closes</li>
+						<li><span>1 PM: Lunch – Grab a bite to eat and don’t forget to hydrate!</span></li>
 
-						<li>9 AM: HACKATHON BEGINS – GET HACKING!
-							<br/>Registration closes
-							<ul>
-								<li>1 PM: Lunch – Grab a bite to eat and don’t forget to hydrate!</li>
+						<li><span>5 PM: Dinner – You’ll want a little extra fuel for these last few hours!</span></li>
 
-								<li>5 PM: Dinner – You’ll want a little extra fuel for these last few hours!</li>
+						<li><span>7 PM: HACKATHON ENDS</span>
+						<br/><br/>Have project submitted  and demo ready to present</li>
 
-								<li>7 PM: HACKATHON ENDS
-								<br/>Have project submitted  and demo ready to present</li>
+						<li><span>7-7:30 PM: Presentation of demos</span>
+						<br/><br/>Check out the creativity  of your fellow hacking peers!</li>
 
-								<li>7-7:30 PM: Presentation of demos
-								<br/>Check out the creativity  of your fellow hacking peers!</li>
+						<li><span>7:30-7:45 PM: Judging Period</span></li>
 
-								<li>7:30-7:45 PM: Judging Period</li>
+						<li><span>7:45-8:00 PM: Winners Announced and Presentation of Awards</span></li>
 
-								<li>7:45-8:00 PM: Winners Announced and Presentation of Awards</li>
-
-								<li>8:00 - ??? GO HOME AND GET SOME SLEEP</li>
-
-							</ul>
-						</li>
+						<li><span>8:00 - ??? GO HOME AND GET SOME SLEEP</span></li>
 					</ul>
-
 		    	</div>
 		    </div>
 		</div>
