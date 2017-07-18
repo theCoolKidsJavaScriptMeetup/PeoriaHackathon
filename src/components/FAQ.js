be474e4010dbe978dd482f5777bbfe367ac7fa62
Peter Kobak <peterkobak@gmail.com>
2017-07-18 11:52:33 -0500

    Updated Content

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -20,13 +20,13 @@ export default class FAQ extends Component {
                     <FAQCard title="What if I don't code?" answer="Coding is a necessary part of this event, but the hackathon is open to all levels of experience. If you are new to coding, it is recommended that you join or create a team with experienced coders."/>
                     <FAQCard title="What are the prizes?" answer={<div>1st Prize: $1,000<br/>2nd Prize: $500</div>}/>
                     <FAQCard title="Are there travel reimbursements?" answer="Unfortunately, we will not be able to provide travel reimbursements to attendees."/>
-                    <FAQCard title="When should I arrive?" answer={<div><div>Friday 8/11: <i>(Optional)</i> Join us between 6:00PM-8:00PM for a social event. This is a good opportunity to create or join a team and socialize with fellow participants.<br/><br/>Saturday 8/12: Check-in and registration will take place between 8AM-8:30AM on Saturday morning.</div></div>}/>
+                    <FAQCard title="When should I arrive?" answer={<div><div>Friday 8/11: <i>(Optional)</i> Join us between 6:00PM-8:00PM for a social event. This is a good opportunity to create or join a team and socialize with fellow participants. (Location TBD)<br/><br/>Saturday 8/12: Check-in and registration will take place between 8AM-8:30AM on Saturday morning in the Peoria Civic Center Verizon Lobby.</div></div>}/>
                     <FAQCard title="Do I need a team?" answer="Teams may be formed before or at the beginning of the event; there will be an opportunity to find a team during Friday’s social event. Recommended team size is 2-7 people."/>
                     <FAQCard title="How do I register a team?" answer={<div>Please head over to the <Link to="/registration">team registration page!</Link></div>}/>
                     <FAQCard title="Will there be food?" answer="All meals, snacks, and drinks will be provided during Saturday's event."/>
                     <FAQCard title="Who do I contact with questions before the event?" answer={<div><div>Please contact event organizer <a href="mailto:pkobak@peoriagov.org">Peter Kobak</a> with any questions prior to the hackathon.</div></div>}/>
                     <FAQCard title="Will you have hardware?" answer="This will be a BYOD (Bring Your Own Device) hackathon. No hardware will be provided."/>
-                    <FAQCard title="What is expected of participants?" answer="Registered participants have agreed to the Terms of Use and are expected to follow the Peoria Civic Hackathon Code of Conduct."/>
+                    <FAQCard title="What is expected of participants?" answer="Registered participants have agreed to the Terms of Use (Link to Terms of Use) and are expected to follow the Peoria Civic Hackathon Code of Conduct (Link to Code of Conduct)."/>
                 </div>
             </div>
         );
--- src/components/Main.js
+++ src/components/Main.js
@@ -9,29 +9,28 @@ export default class Main extends Component {
 	        <div className="Content Content-home">
 		    	<div className="intro_message">
 			    	<h3>On Saturday, August 12th, 2017 The City of Peoria will host the area’s first civic hackathon to engage the local tech community for the purpose of innovating new civic tech solutions.</h3>
-			    	<p>Teams of 2-7 participants will utilize City data while competing to create a website, app, or other tool that could improve the lives of people who live, work, and play in the Peoria area.</p>
-			    	<p>Peoria Civic Hackathon 2017 is taking place in conjunction with Ignite Peoria, a day-long event at the Peoria Civic Center that attracts thousands of visitors to interact with and learn about the thriving arts and tech community in the region.</p>
+			    	<p>Teams of 2-7 participants will utilize City data while competing to create a website, app, or other tool that could improve the lives of people who live, work, and play in the Peoria area. Peoria Civic Hackathon 2017 is taking place in conjunction with Ignite Peoria, a day-long event at the Peoria Civic Center that attracts thousands of visitors to interact with and learn about the thriving arts and tech community in the region.</p>
 		    	</div>
 
 		    	<div className="agenda">
 		    		<h2>Agenda</h2>
-		    		<h3>Friday Night Meet @ ___ (local watering hole)</h3>
+		    		<h3>Friday Night Meet @ <i>TBD</i></h3>
 			    	<ul>
 			    		<li><span>6-6:30 PM: Check-in, pick up a name tag, and grab a drink (if you wish)!</span></li>
 
-						<li><span>6:30-7 PM: Welcome from the i-team and Q & A session about the Hackathon</span>
-						<br/><br/>Use this time to gather with your team or find a team if you are a solo participant looking to join a team. The i-team will also be open to answering any questions about the hackathon during this time</li>
+						<li><span>6:30-7 PM: Welcome from the City of Peoria and Q & A session about the Hackathon</span>
+						<br/><br/>Use this time to gather with your team or find a team if you are a solo participant looking to join a team. Members from the City of Peoria Innovation Team will also be open to answering any questions about the hackathon during this time</li>
 
 						<li><span>7-8 PM: Mingle with fellow hackers!</span>
 						<br/><br/>Use this time to brainstorm with your team, welcome new members to your team, or just meet some new people and have fun!</li>
 					</ul>
-					<h3>Saturday Meet @ the Civic Center Wireless Lobby</h3>
+					<h3>Saturday Meet @ <i>Civic Center Verizon Lobby</i></h3>
 					<ul>
 						<li><span>8-8:30 AM: Check-in and Breakfast</span>
-						<br/><br/>Stop at the registration table to check-in, grab a name tag, make sure all members of your team are registered, and find out where your team’s work space is located. Teams will have the opportunity to move around once the Hackathon period begins. Be sure to grab some  breakfast and check out the SWAG table!</li>
+						<br/><br/>Stop at the registration table to check-in, grab a name tag, make sure all members of your team are registered, and find out where your team’s work space is located. Teams will have the opportunity to move around once the Hackathon period begins.</li>
 
 						<li><span>8:30-9 AM: Opening Remarks from the i-team and City of Peoria IT Department</span>
-						<br/><br/>A word from our sponsors. Introduction of the  Hackathon Challenge Statement</li>
+						<br/><br/>Introduction of the  Hackathon Challenge Statement and a word from our sponsors. </li>
 
 						<li><span>9 AM: HACKATHON BEGINS – GET HACKING!</span>
 							<br/><br/>Registration closes</li>
@@ -40,16 +39,14 @@ export default class Main extends Component {
 						<li><span>5 PM: Dinner – You’ll want a little extra fuel for these last few hours!</span></li>
 
 						<li><span>7 PM: HACKATHON ENDS</span>
-						<br/><br/>Have project submitted  and demo ready to present</li>
+						<br/><br/>At 7 PM, your project must be submitted and your team must be ready to demonstrate its functionality to the judges panel.</li>
 
 						<li><span>7-7:30 PM: Presentation of demos</span>
-						<br/><br/>Check out the creativity  of your fellow hacking peers!</li>
+						<br/><br/>Present your projects and see what other teams were able to put together!</li>
 
 						<li><span>7:30-7:45 PM: Judging Period</span></li>
 
 						<li><span>7:45-8:00 PM: Winners Announced and Presentation of Awards</span></li>
-
-						<li><span>8:00 - ??? GO HOME AND GET SOME SLEEP</span></li>
 					</ul>
 		    	</div>
 		    </div>
