07aba6e361bf159034977a25a980e4ffbbe95430
Alexander Castor <ancastor94@gmail.com>
2017-07-20 15:14:44 -0500

    restructured sponsor page

--- src/components/Sponsors.js
+++ src/components/Sponsors.js
@@ -7,53 +7,78 @@ import goodenergylogo from '../assets/GoodEnergyLogo.png';
 import CoPlogo from '../assets/CoP-logo.png';
 import sponsorshipPacket from '../assets/SponsorshipPacket.pdf'
 
-const SponsorCard = ({sponsorName, sponsorImage, sponsorImageAlt}) => {
-		return (
-			<div className="sponsorContainer">
-				<div>
-					<img className="sponsorImage" src={sponsorImage} alt={sponsorImageAlt}/>
-				</div>
+const SponsorCard = ({sponsorImage, sponsorImageAlt}) => {
+	return (
+		<div key={sponsorImageAlt} className="sponsorContainer">
+			<div>
+				<img className="sponsorImage" src={sponsorImage} alt={sponsorImageAlt}/>
 			</div>
-		);
+		</div>
+	);
 }
 
+const SponsorLevelCard = ({sponsorLevel, sponsors}) => { 
+	return (
+		<div className="sponsorLevelContainer">
+			<h2>{sponsorLevel}</h2>
+			{sponsors.map(sponsor => (
+				<SponsorCard sponsorImage={sponsor.sponsorImage} sponsorImageAlt={sponsor.sponsorImageAlt} />
+			))}
+		</div>
+	);
+}
+
+const goldSponsors = [
+	{
+		sponsorImage: goodenergylogo,
+		sponsorImageAlt: "Good Energy Logo"
+	}
+]
+
+const silverSponsors = [
+	{
+
+	}
+]
+
+const platinumSponsor = [
+	{
+		sponsorImage: devBrightLogo,
+		sponsorImageAlt: "DevBright Logo"
+	}
+]
+
+const partners = [
+	{
+		sponsorImage:CoPlogo,
+		sponsorImageAlt:"City of Peoria Logo"
+	},
+	{
+		sponsorImage: civicCenterlogo,
+		sponsorImageAlt: "Civic Center Logo"
+	}, 
+	{
+		sponsorImage: rclLogo,
+		sponsorImageAlt:"River City Labs"
+	}, 
+	{
+		sponsorImage: artsPartnerslogo,
+		sponsorImageAlt:"Arts Partners Logo"
+}]
 export default class Sponsors extends Component {
 		render() {
 				return (
 						<div className="Content center-text">
-								<h2>Platinum Sponsor</h2>
-								<SponsorCard
-									sponsorImage={devBrightLogo}
-									sponsorImageAlt="DevBright Logo"/>
-								<h2>Gold Sponsors</h2>
-								<div className="sponsorsContainer">
-										<SponsorCard
-												sponsorImage={goodenergylogo}
-												sponsorImageAlt="Good Energy Logo"/>
-								</div>
-								<div className="sponsorsContainer"></div>
-								<h2>Silver Sponsors</h2>
-								<p>Spots still available!</p>
-								<div className="sponsorsContainer"></div>
-								<h3>In Partnership With Ignite Peoria and...</h3>
-								<div className="partnersContainer">
-										<SponsorCard
-												sponsorImage={CoPlogo}
-												sponsorImageAlt="City of Peoria Logo"/>
-												
-										<SponsorCard
-												sponsorImage={civicCenterlogo}
-												sponsorImageAlt="Civic Center Logo"/>
-								
-										<SponsorCard
-												sponsorImage={rclLogo}
-												sponsorImageAlt="River City Labs"/>
-								
-										<SponsorCard
-												sponsorImage={artsPartnerslogo}
-												sponsorImageAlt="Arts Partners Logo"/>
-								</div>
-								<div className="interestedSponsor">Interested in becoming a sponsor of the Peoria Civic Hackathon? <a href={sponsorshipPacket} target="_blank" rel="noopener noreferrer" className="whiteLink">See here for details.</a></div>
+							<SponsorLevelCard sponsorLevel="Platinum Sponsor" sponsors={platinumSponsor} />
+							<SponsorLevelCard sponsorLevel="Gold Sponsors" sponsors={goldSponsors} />
+							<p>Spots still available!</p>
+							<SponsorLevelCard sponsorLevel="Silver Sponsors" sponsors={silverSponsors} />
+							<p>Spots still available!</p>
+															
+							<div className="partnersContainer">
+								<SponsorLevelCard sponsorLevel="Partners" sponsors={partners} />
+							</div>
+							<div className="interestedSponsor">Interested in becoming a sponsor of the Peoria Civic Hackathon? <a href={sponsorshipPacket} target="_blank" rel="noopener noreferrer" className="whiteLink">See here for details.</a></div>
 						</div>
 				);
 		}
