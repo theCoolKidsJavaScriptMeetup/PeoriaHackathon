3720f21dfcd80ae7726f90b2e3281551791d8eae
Peter Kobak <peterkobak@gmail.com>
2017-07-20 09:32:04 -0500

    Added partners to Sponsors page

--- src/components/Sponsors.js
+++ src/components/Sponsors.js
@@ -1,18 +1,21 @@
 import React, {Component} from 'react';
-import devBrightLogo from '../assets/devBrightLogo.png';
+import devBrightLogo from '../assets/DevBright_Logo.png';
+import rclLogo from '../assets/RCL_WHITE.png';
+import civicCenterlogo from '../assets/CivicCenterLogo.png';
+import artsPartnerslogo from '../assets/Arts-Partners-Logo.png';
 import sponsorshipPacket from '../assets/SponsorshipPacket.pdf'
 
 const SponsorCard = ({sponsorName, sponsorImage, sponsorImageAlt}) => {
 		return (
 				<div className="sponsorContainer">
-						<div >
+						<div>
 								<img className="sponsorImage" src={sponsorImage} alt={sponsorImageAlt}/>
 						</div>
-						<div className="flexColumn">
+						 <div className="flexColumn">
 								<div className="sponsorBlurb">
 										<h3 className="blackText">{sponsorName}</h3>
 								</div>
-						</div>
+						</div> 
 				</div>
 		);
 }
@@ -33,8 +36,23 @@ export default class Sponsors extends Component {
 								<h2>Silver Sponsors</h2>
 								<p>Spots still available!</p>
 								<div className="sponsorsContainer"></div>
-								<h3>Interested in becoming a sponsor?</h3>
-								<a href={sponsorshipPacket} target="_blank" rel="noopener noreferrer" className="whiteLink">See here for details.</a>
+								<h3>In Partnership With...</h3>
+								<div className="sponsorsContainer">
+										<SponsorCard
+												sponsorImage={civicCenterlogo}
+												sponsorImageAlt="Civic Center Logo"/>
+								</div>
+								<div className="sponsorsContainer">
+										<SponsorCard
+												sponsorImage={rclLogo}
+												sponsorImageAlt="River City Labs"/>
+								</div>
+								<div className="sponsorsContainer">
+										<SponsorCard
+												sponsorImage={artsPartnerslogo}
+												sponsorImageAlt="Arts Partners"/>
+								</div>
+								<div className="interestedSponsor">Interested in becoming a sponsor of the Peoria Civic Hackathon? <a href={sponsorshipPacket} target="_blank" rel="noopener noreferrer" className="whiteLink">See here for details.</a></div>
 						</div>
 				);
 		}
