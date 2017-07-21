560cb1a679c360f49a32a1e98a80b0a824b846bc
Daniel Hart <me@danielhart.co>
2017-07-21 12:35:10 -0500

    Added CKJM link in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,6 +9,10 @@ export default class Footer extends Component {
       <div className="Footer">
         <a className="footerText" href="https://twitter.com/search?f=tweets&q=codeforpeoria&src=typd" target="blank">#codeforpeoria</a>
        <a href="http://www.peoriagov.org/" target="blank"><img src={CoPlogo} alt="City of Peoria Logo" /></a>
+
+       <div className="createdBy">
+       	<a target="_blank" href="https://github.com/theCoolKidsJavaScriptMeetup">Made with <i className="fa fa-heart"></i> by The Cool Kids Javascript Meetup</a>
+       </div>
       </div>
     );
   }
