079d684b5c6539a6c04908037f52874a220b5e56
Peter Kobak <peterkobak@gmail.com>
2017-07-21 16:26:57 -0500

    re-arranged #codeforpeoria in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,9 +7,9 @@ export default class Footer extends Component {
   render() {
     return (
       <div className="Footer">
-        <a className="footerText" href="https://twitter.com/search?f=tweets&q=codeforpeoria&src=typd" target="blank">#codeforpeoria</a>
+        
        <a href="http://www.peoriagov.org/" target="blank"><img src={CoPlogo} alt="City of Peoria Logo" /></a>
-
+<a className="footerText" href="https://twitter.com/search?f=tweets&q=codeforpeoria&src=typd" target="blank">#codeforpeoria</a>
        <div className="createdBy">
        	<a target="_blank" href="https://github.com/theCoolKidsJavaScriptMeetup">Made with <i className="fa fa-heart"></i> by The Cool Kids Javascript Meetup</a>
        </div>
