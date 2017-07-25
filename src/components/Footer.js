c7ff23eb350af4ffe37c38eb1733057211cba42c
Peter Kobak <peterkobak@gmail.com>
2017-07-24 19:42:11 -0500

    Content Changes
    - Challenge Statement FAQ
    - Bit on Challenge Statement on Main Page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,10 +8,10 @@ export default class Footer extends Component {
     return (
       <div className="Footer">
         
-       <a href="http://www.peoriagov.org/" target="blank"><img src={CoPlogo} alt="City of Peoria Logo" /></a>
+       <a href="http://www.peoriagov.org/" target="blank" rel="noopener noreferrer"><img src={CoPlogo} alt="City of Peoria Logo" /></a>
 <a className="footerText" href="https://twitter.com/search?f=tweets&q=codeforpeoria&src=typd" target="blank">#codeforpeoria</a>
        <div className="createdBy">
-       	<a target="_blank" href="https://github.com/theCoolKidsJavaScriptMeetup">Made with <i className="fa fa-heart"></i> by The Cool Kids Javascript Meetup</a>
+       	<a target="_blank" href="https://github.com/theCoolKidsJavaScriptMeetup" rel="noopener noreferrer">Made with <i className="fa fa-heart"></i> by The Cool Kids Javascript Meetup</a>
        </div>
       </div>
       
