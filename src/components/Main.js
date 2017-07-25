8d8d9b8f6aa80a8d28651bb77d61bc751df70e88
Peter Kobak <peterkobak@gmail.com>
2017-07-25 15:59:56 -0500

    Stylized elements below header on front page

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,9 +8,14 @@ export default class Main extends Component {
     	<div className="color-cover">
 	    	<Header />
 	        <div className="Content Content-home">
+				<div className="content-bar"><div className="content-bar-text">On Saturday, August 12th, 2017, The City of Peoria will host the area’s first civic hackathon to engage the local tech community for the purpose of innovating new civic tech solutions.</div></div>
 		    	<div className="intro_message">
-			    	<h3>On Saturday, August 12th, 2017, The City of Peoria will host the area’s first civic hackathon to engage the local tech community for the purpose of innovating new civic tech solutions.</h3>
-			    	<p>Teams of 2-7 participants will utilize City data while competing to create a website, app, or other tool that could improve the lives of people who live, work, and play in the Peoria area. Presented at the Hackathon will be a Challenge Statement and accompanying data set(s) that may be relevant to creating solutions that address the challenge.</p><p> Peoria Civic Hackathon 2017 is taking place in conjunction with Ignite Peoria, a day-long event at the Peoria Civic Center that attracts thousands of visitors to interact with and learn about the thriving arts and tech community in the region.</p>
+					<p><i> Peoria Civic Hackathon 2017 is taking place in conjunction with Ignite Peoria, a day-long event at the Peoria Civic Center that attracts thousands of visitors to interact with and learn about the thriving arts and tech community in the region.</i></p>	
+			    	<div className = "FaqCard">
+        <div className="FaqQuestion">What is a Civic Hackathon?</div>
+        <div className= "FaqAnswer"><p>Individuals or teams of 2-7 participants will come together for a coding marathon to solve a local challenge. After arriving to the Hackathon, participants will receive a Challenge Statement and access to relevant data sets. They will then be free to utilize this data, as well as other publicly available data, to create a website, app, or other tool that could improve the lives of people who live, work, and play in the Peoria area.</p><p>After the end of the 10 hour hacking session, teams will demo their projects to a panel of judges, who will decide which 2 teams created the best solution to the Challenge Statement during the hackathon. The winning and runner-up team will then be awarded cash prizes to celebrate their work.</p></div>
+    </div>
+			    
 		    	</div>
 
 		    	<div className="agenda">
