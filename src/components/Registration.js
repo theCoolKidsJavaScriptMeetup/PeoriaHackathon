915a49ef695a1ca23e40f91cebf72c500d082f21
Peter Kobak <peterkobak@gmail.com>
2017-08-02 14:13:29 -0500

    Added notes and icons to Registration page

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -9,7 +9,7 @@ const RegistrationButton = ({path, helpText, buttonText}) => {
         <Link className="whitebutton button" to={path}>{buttonText}</Link>
         <i className="fa fa-info-circle helpIcon" aria-hidden="true" title={helpText}></i>
       </div>
-      <p className="helpText">{helpText}</p>
+       <p className="helpText"><i className="fa fa-info-circle"></i> {helpText}</p>
   </div>
   );
 }
@@ -19,6 +19,9 @@ export default class Registration extends Component {
     return (
       <div className='Content'>
         <h2>Get Registered!</h2>
+        <div className="registration_intro_message">
+        <p>Registration for the Peoria Civic Hackathon is free. While the event is open to the public, registration is required in order to participate and be eligible for the prize.</p>
+        </div>
         <div className="button-group stacked">
            <RegistrationButton path="/register/individual" buttonText="Register as an Individual" helpText="Register as an individual if you haven't formed a team yet with others. You can always join or create a team later. Attend our Hackathon Social on Friday, August 11th to meet other participants and form a team."/>
            <RegistrationButton path="/registerTeam/create" buttonText="Create a Team" helpText="Create a team and the password needed for others to join it. Afterwards, you'll be prompted to register with that team."/>
